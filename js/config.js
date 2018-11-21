d5eb3a2fdf5a3490b7f9c487953af3183c50320e
Chris Diana <cdiana.media@gmail.com>
2018-11-21 03:16:47 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -11,9 +11,9 @@ var config = {
   // If Github mode is set, your Github username, repo name, 
   // and branch to get files from.
   github: {
-    username: 'yourusername',
-    repo: 'yourrepo',
-    branch: 'gh-pages',
+    username: 'saturnbit',
+    repo: 'cms.js-starter',
+    branch: 'ph-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'subdirectory',
